5f29b32ca6489d9631d1533bc9b7b2e5df6723bd
Norbey <wilson.norbey@gisoft.co>
2023-08-13 16:18:31 -0500

    feat:ajuste para el servicio Qr

--- src/app/Landingpage/Landingpage.component.ts
+++ src/app/Landingpage/Landingpage.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { QrInfoSaleService } from '../dashboard/services/qrInfoSale.service';  // Asegúrate de que la ruta al servicio sea correcta
+import { QrInfoSaleService } from './Services/QrInfoSale.service';
 
 @Component({
   selector: 'app-Landingpage',
@@ -9,31 +9,25 @@ import { QrInfoSaleService } from '../dashboard/services/qrInfoSale.service';  /
 })
 export class LandingpageComponent implements OnInit {
   id_sale: any;
-  sale: any = {}; // Objeto para almacenar la venta
-  caracteristicas: any = {}; // Array para almacenar las características
+  sale: any = {}; 
+  caracteristicas: any = {}; 
 
   constructor(
     private activatedrouter: ActivatedRoute,
-    private qTest: QrInfoSaleService  // Corregir el nombre de la variable de inyección
+    private qTest: QrInfoSaleService 
   ) {}
 
   ngOnInit() {
     this.activatedrouter.params.subscribe(params => {
       this.id_sale = params['id'];
-      // this.getCommerceDetail(params['id']);
-      console.log("param", params['id']);
     });
     this.subco();
   }
 
   subco() {
-    this.qTest.getqr(this.id_sale).subscribe((resp: any) => {  // Usar la variable qTest en lugar de Qtest
-      this.sale = resp; // Asignamos los datos a la variable
-     // this.caracteristicas = JSON.parse(this.sale.caracteristicas);
-      this.caracteristicas = JSON.parse(this.sale.caracteristicas)[0];
-      console.log("resp", this.caracteristicas);
-      //this.element = ({uuid2: resp.data.raffle.game.name, uuid: resp.data.amount, transaction_status_name: resp.data.stateSale.description, created_at: resp.data.createdAt, prizeWon: resp.data.prizeWon});
-      //console.log("this.element", this.element);
+    this.qTest.getqr(this.id_sale).subscribe((resp: any) => {  
+      this.sale = resp;
+       this.caracteristicas = JSON.parse(this.sale.caracteristicas)[0];
     });
   }
 }
